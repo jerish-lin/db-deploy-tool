144ffd7ea9a5fde25bf3c7a3f7808830583cd735
jerish.lin <jerish.lin@outlook.com>
2026-01-21 12:17:59 +0800

    exclude DataSourceAutoConfiguration.class

--- src/main/java/org/jerish/dbdeploy/DatabaseDeployTool.java
+++ src/main/java/org/jerish/dbdeploy/DatabaseDeployTool.java
@@ -10,9 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @Slf4j
 public class DatabaseDeployTool implements CommandLineRunner {
     @Autowired
